61090a91d0716cf40606f10d2b696e07cb803f31
damronathan <damronnathan14@gmail.com>
2024-07-16 20:14:43 -0700

    Reset to previous commit

---
+++ TaskManagerDB/dbo/Tables/Tasks.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TaskItems]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1), 
+    [Title] NVARCHAR(100) NOT NULL, 
+    [Description] NVARCHAR(500) NULL, 
+    [IsComplete] BIT NOT NULL
+)

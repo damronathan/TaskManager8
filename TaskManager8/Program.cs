0b85dc4257e2ca1342f4e3f2963d6e5642eb0ec7
damronathan <damronnathan14@gmail.com>
2025-04-01 07:15:55 -0500

    refactored task service to be interface

--- TaskManager8/Program.cs
+++ TaskManager8/Program.cs
@@ -1,66 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using TaskManagerApp.Services;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+﻿using TaskManagerApp.Services;
 using TaskManagerLibrary.Services;
-using TaskManagerLibrary.Models;
 using System.Diagnostics;
-namespace TaskManagerApp
+
+namespace TaskManagerApp;
+
+
+class Program
 
 {
-    class Program
-
+    
+    static async Task Main(string[] args)
     {
+        var taskService = new TaskService();
+        var taskMethods = new TaskMethods(taskService);
 
-        static async Task Main(string[] args)
+        // Get all tasks
+        var tasks = await taskService.GetTasksAsync();
         {
-            var taskService = new ITaskService();
-            var taskMethods = new TaskMethods(taskService);
+            Console.WriteLine("TASK MANAGER");
+            List<string> menu = new List<string>
+        {
+            "",
+            "1. Add Task",
+            "2. View Tasks",
+            "3. Mark Task as Completed",
+            "4. Update Task",
+            "5. Delete Task",
+            "6. Exit"
+        };
 
-            // Get all tasks
-            var tasks = await taskService.GetTasksAsync();
+            while (true)
             {
-                Console.WriteLine("TASK MANAGER");
-                List<string> menu = new List<string>
-            {
-                "",
-                "1. Add Task",
-                "2. View Tasks",
-                "3. Mark Task as Completed",
-                "4. Update Task",
-                "5. Delete Task",
-                "6. Exit"
-            };
-
-                while (true)
+                int selection = taskMethods.DisplayMenuAndGetSelection(menu);
+                switch (selection)
                 {
-                    int selection = taskMethods.DisplayMenuAndGetSelection(menu);
-                    switch (selection)
-                    {
-                        case 1:
-                            await taskMethods.AddTaskAsync();
-                            break;
-                        case 2:
-                            await taskMethods.ViewTasksAsync();
-                            break;
-                        case 3:
-                            await taskMethods.MarkCompleteAsync();
-                            break;
-                        case 4:
-                            await taskMethods.UpdateTaskAsync();
-                            break;
-                        case 5:
-                            await taskMethods.DeleteTaskAsync();
-                            break;
-                        case 6:
-                            Process[] myProcList = Process.GetProcessesByName("TaskManagerWebApp");
-                            foreach (Process Target in myProcList)
-                            {
-                                Target.Kill();
-                            }
-                            return;
-                    }
+                    case 1:
+                        await taskMethods.AddTaskAsync();
+                        break;
+                    case 2:
+                        await taskMethods.ViewTasksAsync();
+                        break;
+                    case 3:
+                        await taskMethods.MarkCompleteAsync();
+                        break;
+                    case 4:
+                        await taskMethods.UpdateTaskAsync();
+                        break;
+                    case 5:
+                        await taskMethods.DeleteTaskAsync();
+                        break;
+                    case 6:
+                        Process[] myProcList = Process.GetProcessesByName("TaskManagerWebApp");
+                        foreach (Process Target in myProcList)
+                        {
+                            Target.Kill();
+                        }
+                        return;
                 }
             }
         }
